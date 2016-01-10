dfc23b25c80e81e017ec6e06b7ae3da4e28fe79d
Conor C <Conorc1000@gmail.com>
2016-01-10 11:54:47 +0000

    able to upload img locally

--- server.js
+++ server.js
@@ -1,7 +1,47 @@
-var express = require('express'),
-app = express();
+var body_parser = require('body-parser');
+var http = require('http');
+var path = require('path');
+var aws = require('aws-sdk');
+
+var express = require('express');
+var app = express();
 app.use(express.static('www'));
 app.set('port', process.env.PORT || 5000);
-app.listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + app.get('port'));
+
+var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
+
+console.log(AWS_SECRET_KEY);
+
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+app.get('/sign_s3', function(req, res) {
+  console.log('query========================>>>>>>>>>>>>', req.query);
+  console.log(AWS_SECRET_KEY);
+  aws.config.update({
+    accessKeyId: 'AKIAJ37FIF773HO2ZVWA',
+    secretAccessKey: AWS_SECRET_KEY
+  });
+  var s3 = new aws.S3();
+  var s3_params = {
+    Bucket: 'boatlaunchslipwayphotos',
+    Key: 'WebSitePhotos/' + req.query.file_name,
+    Expires: 60,
+    ContentType: req.query.file_type,
+    ACL: 'public-read'
+  };
+
+  s3.getSignedUrl('putObject', s3_params, function(err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      var return_data = {
+        signed_request: data,
+        url: 'https://' + 'boatlaunchslipwayphotos' + '.s3.amazonaws.com/' + req.query.imgId
+      };
+      res.write(JSON.stringify(return_data));
+      res.end();
+    }
+  });
 });
--- www/js/services/ImgUploadService.js
+++ www/js/services/ImgUploadService.js
@@ -5,68 +5,85 @@ boatlaunchServices
   var uniqueImageNumber = 0;
 
   AWS.config.update({
-    accessKeyId: 'AKIAIC55N4PSACLZW5DA',
-    secretAccessKey: 'TODO: EDIT'
+    accessKeyId: 'AKIAJ37FIF773HO2ZVWA'
   });
   AWS.config.region = 'eu-west-1'; //could also be Ireland
 
-
+  // Initialize the Amazon Cognito credentials provider
+  AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: 'eu-west-1:06a62dcc-2d0e-44e3-a400-779cc16b2ef0',
+  });
 
 
   var uploadImg = function(fileChooser, results, slipway) {
-    var maxImgId;
-    firebaseRef.child("maxImgId").once('value', function(snapshot) {
-      maxImgId = snapshot.val() + 1;
-      firebaseRef.child("maxImgId").set(maxImgId, function(error) {
-        if (error) {
-          console.log('maxImgId Synchronization failed', error);
-        }else{
-          console.log('maxImgId Synchronization succeeded');
 
-          if (!slipway.imgs) {
-            slipway.imgs = [];
-          }
-          slipway.imgs.push(maxImgId);
-          firebaseRef.child("slipwayDetails").child(slipway.idKey).child("imgs").set(slipway.imgs, function(err) {
-            if (error) {
-              console.log('push imgId to firebase failed', error);
-            }else{
-              console.log('push imgId to firebase succeeded');
-              putImgToAWS(maxImgId);
-            }
-          });
-        }
-      });
+    if (!slipway.imgs) {
+      slipway.imgs = [];
+    }
+    var imgId = "New" + Date.now();
+
+    slipway.imgs.push(imgId);
+    firebaseRef.child("slipwayDetails").child(slipway.idKey).child("imgs").set(slipway.imgs, function(err) {
+      if (err) {
+        console.log('push imgId to firebase failed', error);
+      } else {
+        console.log('push imgId to firebase succeeded');
+        putImgToAWS(imgId, fileChooser);
+      }
     });
 
-    function putImgToAWS(maxImgId) {
-
-      var bucket = new AWS.S3({
-        params: {
-          Bucket: 'boatlaunchimages',
-          Key: "WebSitePhotos/" + maxImgId + "___Source.jpg",
-          ACL: "public-read"
-        }
-      });
-
-      var file = fileChooser.files[0];
-      console.log(file);
-      if (file) {
-        results.innerHTML = '';
-
-        var params = {
-          ContentType: file.type,
-          Body: file
-        };
-        bucket.upload(params, function(err, data) {
-          console.log("data-------->>>>>>>>>", data);
-          results.innerHTML = err ? err + 'ERROR!' : 'UPLOADED.';
-        });
-      } else {
-        results.innerHTML = 'Nothing to upload.';
-      }
-    }
   };
+
+  function putImgToAWS(imgId, fileChooser) {
+
+    var file = fileChooser.files[0];
+
+    console.log(file);
+    if (file) {
+      results.innerHTML = '';
+
+      get_signed_request(file, imgId);
+
+    } else {
+      results.innerHTML = 'Nothing to upload.';
+    }
+  }
+
+  function get_signed_request(file, imgId) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "/sign_s3?file_name=" + imgId + "___Source.jpg" + "&file_type=" + file.type);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          var response = JSON.parse(xhr.responseText);
+          upload_file(file, response.signed_request, response.url);
+        } else {
+          alert("Could not get signed URL.");
+        }
+      }
+    };
+    xhr.send();
+  }
+
+  function upload_file(file, signed_request, url) {
+    console.log('upload_file, url', url);
+    var xhr = new XMLHttpRequest();
+    xhr.open("PUT", signed_request);
+    xhr.setRequestHeader('x-amz-acl', 'public-read');
+    xhr.onload = function(loadResponse) {
+      if (xhr.status === 200) {
+        console.log('xhr.status === 200');
+        console.log('loadResponse', loadResponse);
+        // document.getElementById("preview").src = url;
+        // document.getElementById("avatar_url").value = url;
+      }
+    };
+    xhr.onerror = function() {
+      alert("Could not upload file.");
+    };
+    xhr.send(file);
+  }
+
   return {
     uploadImg: uploadImg
   };
